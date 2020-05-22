041d51f3641e3901bfd6690009fa26a1ba403bda
demola07 <45898468+demola07@users.noreply.github.com>
2020-05-22 22:41:03 +0100

    format amount with commas

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 
 import { GlobalContext } from '../context/GlobalState';
+import { numberWithCommas } from '../utils/format';
 
 const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
@@ -11,7 +12,7 @@ const Transaction = ({ transaction }) => {
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
       {transaction.text}{' '}
       <span>
-        {sign} ₦ {Math.abs(transaction.amount)}
+        {sign} ₦ {numberWithCommas(Math.abs(transaction.amount))}
       </span>
       <button className='delete-btn' onClick={() => deleteTransaction(transaction._id)}>
         x
